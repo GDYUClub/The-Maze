31c744959e56be5d3114751d7357a252c6e5dcd4
JUB_Yoush <yoush1337@gmail.com>
2024-03-17 10:56:42 -0400

    tile collisions, reset ray before checking again

--- src/player/player.gd
+++ src/player/player.gd
@@ -2,18 +2,17 @@ extends Area2D
 
 @onready var raycast := $RayCast2D
 
-var input_dir:= Vector2.ZERO
-const TILE_SIZE:= 16
-
+const TILE_SIZE := 16
 
 func _init() -> void:
-	add_to_group('player')
+	add_to_group("player")
 
-func get_input_vector(event:InputEvent) -> Vector2:
+func _ready() -> void:
+	raycast.collide_with_areas = true
+
+func get_input_vector(event: InputEvent) -> Vector2:
 	# make vector representing player input
 	var dir := Vector2.ZERO
-	#dir.x = Input.get_axis("ui_left","ui_right")
-	#dir.y = Input.get_axis("ui_up","ui_down")
 	if event.is_action_pressed("ui_up"):
 		dir.y -= 1
 	if event.is_action_pressed("ui_down"):
@@ -22,18 +21,24 @@ func get_input_vector(event:InputEvent) -> Vector2:
 		dir.x -= 1
 	if event.is_action_pressed("ui_right"):
 		dir.x += 1
-	print(dir)
 	return dir
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	input_dir = get_input_vector(event)
+	var _input_dir := get_input_vector(event)
 	#check what group the next tile is in based on your raycast
-	if input_dir == Vector2.ZERO:
+	if _input_dir != Vector2.ZERO:
+		_move(_input_dir)
 		return
-	raycast.target_position = input_dir * TILE_SIZE
-	# should make some sort of tileItem class
-	var next_tile :Object = raycast.get_collider()
+
+
+func _move(_input_dir:Vector2) -> void:
+	raycast.target_position = _input_dir * TILE_SIZE
+	# clear old raycast collison then get new one
+	raycast.force_raycast_update()
+	var next_tile: Object = raycast.get_collider()
+
 	if next_tile != null:
 		print(next_tile)
-	position += input_dir * TILE_SIZE
+	else:
+		position += _input_dir * TILE_SIZE
--- src/scene-tiles/chest/chest.gd
+++ src/scene-tiles/chest/chest.gd
@@ -9,6 +9,3 @@ func _ready() -> void:
 	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
