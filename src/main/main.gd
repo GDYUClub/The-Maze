ee72c04bd9d00b368f66c8868d6acca42f669136
JUB_Yoush <yoush1337@gmail.com>
2024-03-18 21:36:02 -0400

    can't act while text is out, paint chests like tiles

--- src/main/main.gd
+++ src/main/main.gd
@@ -1,1 +1,21 @@
 extends Node2D
+
+@onready var ui:UI = $UI
+@onready var player:Player = $Player
+
+var in_cutscene := false
+
+func _ready() -> void:
+    ui.text_rendered.connect(_start_cutscene)
+    ui.text_removed.connect(_end_cutscene)
+    pass
+
+
+func _start_cutscene() -> void:
+    in_cutscene = true
+    player.is_actionable = false
+
+func _end_cutscene() -> void:
+    in_cutscene = false
+    await get_tree().create_timer(0.01).timeout # 10ms buffer hack to prevent input collision from text advancing and player input, could also solve by checking UI input in player _input event?
+    player.is_actionable = true
--- src/main/ui.gd
+++ src/main/ui.gd
@@ -2,9 +2,26 @@ class_name UI
 extends Control
 
 @onready var textbox:Panel = $Textbox
+signal text_rendered
+signal text_removed
+signal text_advanced
 
-func render_text(new_text):
+
+func render_text(new_text_arr) -> void:
+	#loop through the array and display the lines of text
+	text_rendered.emit()
 	textbox.visible = true
 	var textLabel = $Textbox/Text
-	textLabel.text = new_text
-	pass
+	#i = 0
+	#while i < new_text_arr.length():
+	for line in new_text_arr:
+		textLabel.text = line
+		await text_advanced
+	#await text_advanced
+	textbox.visible = false
+	text_removed.emit()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("adv_text"):
+		text_advanced.emit()
+
