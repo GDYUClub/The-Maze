f463c1b0acea935a5deb4a7aaa88fc8ac8d55cbe
Tigran <122690491+TigranExe@users.noreply.github.com>
2024-03-20 19:59:38 -0400

    Attempt at random rooms

--- src/main/main.gd
+++ src/main/main.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 @onready var ui:UI = $UI
 @onready var player:Player = $Player
+@onready var map = $Map
+
+#Packed tilemaps!
+var tilemap_array: Array[PackedScene] = []
 
 var in_cutscene := false
 
@@ -9,6 +13,14 @@ func _ready() -> void:
 	ui.text_rendered.connect(_start_cutscene)
 	ui.text_removed.connect(_end_cutscene)
 	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		var current_tilemap = map.get_child(0)
+		current_tilemap.queue_free()
+		###Do something with random tilemaps?
+		pass
+		
 
 
 func _start_cutscene() -> void:
