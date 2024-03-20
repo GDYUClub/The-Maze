d11dade0c1c1cc002c76fab1f5f9c9d49d1b1d89
JUB_Yoush <yoush1337@gmail.com>
2024-03-19 23:02:07 -0400

    room shuffling and a fake ui

--- src/main/main.gd
+++ src/main/main.gd
@@ -2,13 +2,40 @@ extends Node2D
 
 @onready var ui:UI = $UI
 @onready var player:Player = $Player
+@onready var current_map:Node2D = $Map
 
 var in_cutscene := false
+
+var maps:Dictionary = {0:preload('res://src/maps/map-1.tscn'),1:preload('res://src/maps/map-2.tscn'),2:preload('res://src/maps/map-3.tscn')}
+var shuffled_maps:Array = []
+
+var current_map_id
 
 func _ready() -> void:
     ui.text_rendered.connect(_start_cutscene)
     ui.text_removed.connect(_end_cutscene)
+    player.walked_into_stairs.connect(swap_map)
     pass
+
+func swap_map() -> void:
+    #reshuffle rooms if we've visted them all
+    if shuffled_maps.size() == 0:
+        shuffled_maps = Array(range(maps.size()))
+        shuffled_maps.shuffle()
+    #pick new room from remaining choices
+    var next_map_id :int = shuffled_maps.pop_front()
+    var nextMap:Node2D = maps[next_map_id].instantiate()
+
+    #load new map
+    current_map.queue_free()
+    add_child(nextMap)
+    current_map = nextMap
+
+    #move the player to the spawn point
+    var playerSpawnMarker:Marker2D = nextMap.get_node('PlayerSpawn')
+    player.position = playerSpawnMarker.position
+
+
 
 
 func _start_cutscene() -> void:
---
+++ src/scene-tiles/stairs/stairs.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_to_group('stairs')
+	pass # Replace with function body.
+
