69d141e93dd6e3c9deac2447d11dc9ec71d9fe3e
JUB_Yoush <yoush1337@gmail.com>
2024-03-21 10:55:21 -0400

    manually added Tigrans contributions

--- src/maps/map.gd
+++ src/maps/map.gd
@@ -1,9 +1,10 @@
 class_name Map
 extends Node2D
 
-
+@onready var tilemap:TileMap = $TileMap
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	tilemap.add_to_group('wall')
 	pass # Replace with function body.
 
 
--- src/player/player.gd
+++ src/player/player.gd
@@ -1,12 +1,16 @@
 class_name Player
 extends Area2D
 
+@onready var boots_packed_scene: PackedScene = preload("res://src/boots/boots.tscn")
 @onready var raycast := $RayCast2D
 
 signal walked_into_stairs
+signal made_successful_move(successful_input_dir)
 
 const TILE_SIZE := 16
 var is_actionable:= true
+
+var has_boots := false
 
 func _init() -> void:
 	add_to_group("player")
@@ -48,14 +52,20 @@ func _move(_input_dir:Vector2) -> void:
 	raycast.force_raycast_update()
 	var next_tile: Object = raycast.get_collider()
 
-	if next_tile != null:
-		print(next_tile)
-
-	if next_tile == null:
+	#if next_tile != null:
+		#print(next_tile)
+	# if it's a tile you can interact with you can't walk on it
+	# maybe we could use class names or something that dosen't require making new labels for everything (not an issue rn)
+	if next_tile == null or !next_tile.is_in_group('interact'):
 		position += _input_dir * TILE_SIZE
-	elif next_tile.is_in_group('stairs'):
-		walked_into_stairs.emit()
+		made_successful_move.emit(_input_dir)
+		check_next_tile(next_tile)
 
+func check_next_tile(tile: Object) -> void:
+	if tile == null:
+		return
+	if tile.is_in_group('stairs'):
+		walked_into_stairs.emit()
 
 func _inspect() -> void:
 	raycast.force_raycast_update()
@@ -65,7 +75,14 @@ func _inspect() -> void:
 		return
 	if inspect_tile.is_in_group('chest'):
 		inspect_tile._on_inspection()
-		pass
+		#very very very bad fix asap
+		# pass in player into inspection function or something
+		if !has_boots:
+			print('boots')
+			var new_obtained_boots = boots_packed_scene.instantiate()
+			self.add_child(new_obtained_boots)
+			has_boots = true
+			pass
 
 
 	pass
