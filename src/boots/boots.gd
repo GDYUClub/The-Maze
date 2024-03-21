badd01a2e556b85fe002361e7b6e52d7b8f99b5f
Tigran <122690491+TigranExe@users.noreply.github.com>
2024-03-20 22:05:20 -0400

    Shortening
    Variable and function names reduced to 3 word max

--- src/boots/boots.gd
+++ src/boots/boots.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
 
-var sequence_to_reach_golden_piece: Array[String] = ["down","down","down","down","down"]
-var past_moves_relevant_to_code: Array[String] = ["","","","",""]
+var winning_move_sequence: Array[String] = ["down","down","down","down","down"]
+var past_5_moves: Array[String] = ["","","","",""]
 
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
 	print("BOOTS ON!!!")
-	connect_move_signal_to_player()
+	connect_to_player()
 
-func connect_move_signal_to_player() -> void:
+func connect_to_player() -> void:
 	var parent_player_node = get_node("../")
 	# Yeah!! You can get stuff 2 parents up the tree get_node("../../")
 	if parent_player_node != self:
@@ -20,26 +20,26 @@ func connect_move_signal_to_player() -> void:
 func new_move_made(what_direction) -> void:
 	match what_direction:
 		Vector2(0,1):
-			add_most_recent_move("down")
+			note_new_move("down")
 			pass
 		Vector2(0,-1):
-			add_most_recent_move("up")
+			note_new_move("up")
 			pass
 		Vector2(1,0):
-			add_most_recent_move("right")
+			note_new_move("right")
 			pass
 		Vector2(-1,0):
-			add_most_recent_move("left")
+			note_new_move("left")
 			pass
-	check_if_full_code_was_put()
+	has_player_won()
 	pass
 
-func add_most_recent_move(what_direction_string: String) -> void:
-	past_moves_relevant_to_code.pop_front()
-	past_moves_relevant_to_code.append(what_direction_string)
+func note_new_move(what_direction_string: String) -> void:
+	past_5_moves.pop_front()
+	past_5_moves.append(what_direction_string)
 	pass
 
-func check_if_full_code_was_put() -> void:
-	if past_moves_relevant_to_code == sequence_to_reach_golden_piece:
+func has_player_won() -> void:
+	if past_5_moves == winning_move_sequence:
 		print("yayay!! You won!")
 	pass
