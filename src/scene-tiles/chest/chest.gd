55bcbf075fc0984736e85de5fa807ab73b6498b9
JUB_Yoush <yoush1337@gmail.com>
2024-04-11 11:10:27 -0400

    what is happening

--- src/scene-tiles/chest/chest.gd
+++ src/scene-tiles/chest/chest.gd
@@ -23,26 +23,11 @@ func _on_interaction(player:Player) -> void:
 		# we need 404.dialog lol
 		return
 
+	if opened:
+		return
+
+	opened = true
 	DialogueManager.show_example_dialogue_balloon(load("res://src/dialogue/main.dialogue"),"start")
 	$Sprite2D.frame = 1
-
-	#if !opened:
-		#opened = true
-
-		## we should make a generic chest with the ability to give any item (although we'd need to have some sort of item data strucutre)
-		#if !player.has_boots:
-			#print('boots')
-			#var boots = player.boots_packed_scene.instantiate()
-			#player.add_child(boots)
-			#player.has_boots = true
-
-			#ui.render_text(['The chest contained boots.',"They fit you perfectly.",'The chest also contained a note:','"SOMEONE ADD SONGBOOT NOISES ALREADY!!!"',"You don't think the note was addressing you."])
-
-		## added a cheeky placeholder if statement for when the player is already wearing boots and opens a new chest
-		## did it for shits and giggles
-		#elif player.has_boots:
-			#print('boots again?')
-
-			#ui.render_text(['The chest contained the same boots.', "The ones you're already wearing.", 'You don\'t need them right now.'])
 
 
