2b99e40c2b0d94b87c25e9e8da13283b753535bb
JUB_Yoush <yoush1337@gmail.com>
2024-04-11 11:19:31 -0400

    thing should behave?

--- src/autoloads/player_state.gd
+++ src/autoloads/player_state.gd
@@ -1,12 +1,21 @@
 extends Node
 
-#can you use an onready in a autoload? when is an autoload put in the scene???
+# these are functions that can be called in the dialog system
 
 func has_boots():
-	var current_scene = _get_current_scene()
-	var player:Player = current_scene.get_node("Player")
-	return player.has_boots
+    var current_scene = _get_current_scene()
+    var player:Player = current_scene.get_node("Player")
+    return player.has_boots
+
+func _give_boots():
+    var current_scene = _get_current_scene()
+    var player:Player = current_scene.get_node("Player")
+    player.give_boots()
+    var ui = current_scene.get_node("UI")
+    ui.bootIcon.visible = true
+
+
 
 # ion know what this returns
 func _get_current_scene():
-	return get_tree().get_root().get_children()[0]
+    return get_tree().get_root().get_children()[2]
